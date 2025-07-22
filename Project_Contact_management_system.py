450bf8677e8ec6f61ff3da49e395b35dba33d88c
VinayChilukamari <chilukamarivinay@gmail.com>
2025-07-22 22:53:33 +0530

    Changes made for line addition \n

--- Project_Contact_management_system.py
+++ Project_Contact_management_system.py
@@ -21,16 +21,13 @@ def display_contacts():# this funtion displays data available in contacts.json f
 def index_num_inp(text):# this funtion allows only integer input by validating its data type and returns only integer.
     while True:
         try:
-            print()
-            number=int(input(f"Enter the number of the person you want to {text}: "))
+            number=int(input(f"\nEnter the number of the person you want to {text}: "))
             if number<=0 or number>len(people):
-                print()
-                print("Invalid number, index out range")
+                print("\nInvalid number, index out range")
             else:
                 break
         except Exception as e:
-            print()
-            print("Invalid number, enter an integer value")
+            print("\nInvalid number, enter an integer value")
 
     return number
 
@@ -51,8 +48,7 @@ def delete_person(people):# this function allows user to delete specified person
     display_contacts()
         
     if len(people)==0:
-        print()
-        print("Contact list is empty")
+        print("\nContact list is empty")
     else:
         number=index_num_inp('delete') # function to get the index number of the list to delete
         people.pop(number-1)
@@ -61,46 +57,38 @@ def delete_person(people):# this function allows user to delete specified person
 def search_person(people):# this functions allows user to search the person from contacts.json file.
     while True:
         result=[]
-        print()
-        search_by=input("Do you want to search by 'NAME', 'AGE', 'EMAIL-ID'? or enter 'q' to exit: ").lower()
+        search_by=input("\nDo you want to search by 'NAME', 'AGE', 'EMAIL-ID'? or enter 'q' to exit: ").lower()
         if search_by=='name':
-            print()
-            search_name=input("Do you want to search by 'FIRST NAME', 'LAST NAME', or 'FULL NAME'? (first/last/full): ").lower()
+            search_name=input("\nDo you want to search by 'FIRST NAME', 'LAST NAME', or 'FULL NAME'? (first/last/full): ").lower()
             if search_name=='first name' or search_name=='first':
-                print()
-                first_name=input("Enter the 'first name' of the person you want to search: ").strip()
+                first_name=input("\nEnter the 'first name' of the person you want to search: ").strip()
                 for person in people:
                     if first_name == person["name"][0:len(first_name)]:
                         result.append(person)
                 break
             elif search_name =='last name' or search_name=='last':
-                print()
-                last_name=input("Enter the 'last name' of the person you want to search: ").strip()
+                last_name=input("\nEnter the 'last name' of the person you want to search: ").strip()
                 # print(last_name)
                 for person in people:
                     if last_name == person["name"][-len(last_name):]:
                         result.append(person)
                 break
             elif search_name =='full name' or search_name=='full':
-                print()
-                last_name=input("Enter the 'full name' of the person you want to search: ").strip()
+                last_name=input("\nEnter the 'full name' of the person you want to search: ").strip()
                 for person in people:
                     if last_name == person["name"]:
                         result.append(person)
                 break
             else:
-                print()
-                print("Invalid search")
+                print("\nInvalid search")
         elif search_by=='age':
-            print()
-            age_inp=input("Enter the 'age' of the person you want to search: ").strip()
+            age_inp=input("\nEnter the 'age' of the person you want to search: ").strip()
             for person in people:
                 if age_inp==person['age']:
                     result.append(person)
             break
         elif search_by=='email-id':
-            print()
-            email_inp=input("Enter the 'email-ID' of the person you want to search: ").strip()
+            email_inp=input("\nEnter the 'email-ID' of the person you want to search: ").strip()
             for person in people:
                 if email_inp==person['email-ID']:
                     result.append(person)
@@ -109,18 +97,14 @@ def search_person(people):# this functions allows user to search the person from
         elif search_by =='q':
             break
         else:
-            print()
-            print("Invalid search")
+            print("\nInvalid search")
             
         
     if len(result)==0:
-        print()
-        print("Person not found.\nNote: search is case-sensitive")
+        print("\nPerson not found.\nNote: search is case-sensitive")
     else:
-        print()
         for i in result:
-            print(i)
-        print()
+            print('\n',i,'\n',sep='')
             
 def change(people):# this functions allows user to change persos's attribute from contacts.json file.
     display_contacts()
@@ -130,71 +114,57 @@ def change(people):# this functions allows user to change persos's attribute fro
         change_index=number-1
         if change_index==people.index(person):
             while True:
-                print()
-                change_inp=input("Specify what do you want to change? 'NAME', 'AGE', or 'EMAIL-ID'?: ").lower()
+                change_inp=input("\nSpecify what do you want to change? 'NAME', 'AGE', or 'EMAIL-ID'?: ").lower()
                 if change_inp=='name':
                     o_name=people[change_index]['name']
                     u_name=''
-                    print()
-                    change_by=input("Do you want to change by 'FIRST NAME', 'LAST NAME', or 'FULL NAME'? (first/last/full): ").lower()
+                    change_by=input("\nDo you want to change by 'FIRST NAME', 'LAST NAME', or 'FULL NAME'? (first/last/full): ").lower()
                     if change_by=='first name' or change_by=='first':
                         update=o_name.split()
                         update.remove(update[0])
-                        print()
-                        update.insert(0,input("Enter the updated name: ").strip())
+                        update.insert(0,input("\nEnter the updated name: ").strip())
                         u_name=' '.join(update)
                         people[change_index]['name']=u_name
                         json_dump()
-                        print()
-                        print("First name updated")
+                        print("\nFirst name updated")
                         break
                     elif change_by=='last name' or change_by=='last':
                         
                         update=o_name.split()
                         if len(update)==1:
-                            print()
-                            print("Last name does not exist, please change in full name")
+                            print("\nLast name does not exist, please change in full name")
                             break
                         else:
                             update.remove(update[1])
-                            print()
-                            update.insert(1,input("Enter the updated name: ").strip())
+                            update.insert(1,input("\nEnter the updated name: ").strip())
                             u_name=' '.join(update)
                             people[change_index]['name']=u_name
                             json_dump()
-                            print()
-                            print("Last name updated")
+                            print("\nLast name updated")
                             break
                     elif change_by=='full name' or change_by=='full':
-                        print()
-                        u_name=input("Enter the updated name: ").strip()
+                        u_name=input("\nEnter the updated name: ").strip()
                         people[change_index]['name']=u_name
                         json_dump()
-                        print()
-                        print("Name updated")
+                        print("\nName updated")
                         break
                     else:
                         print("Invalid input")
                         
                     
                 elif change_inp=='age':
-                    print()
-                    people[change_index]['age']=input("Enter the updated age: ").strip()
+                    people[change_index]['age']=input("\nEnter the updated age: ").strip()
                     json_dump()
-                    print()
-                    print("Age updated")
+                    print("\nAge updated")
                     break
                     
                 elif change_inp=='email-id':
-                    print()
-                    people[change_index]['email-ID']=input("Enter the updated email-ID: ").strip()
+                    people[change_index]['email-ID']=input("\nEnter the updated email-ID: ").strip()
                     json_dump()
-                    print()
-                    print("Email-ID updated")
+                    print("\nEmail-ID updated")
                     break
                 else:
-                    print()
-                    print("Invalid input, try again")
+                    print("\nInvalid input, try again")
                 
         
 
@@ -204,19 +174,14 @@ people= json_read()
 # print(people)
    
 while True:
-    # size=len(people)
-    print()
-    print(f"Contact list size is: {len(people)}")
-    print()
-    action=input("Do you want to 'VIEW', 'ADD', 'CHANGE', 'DELETE', 'SEARCH' a person, or Enter 'q' to exit: ").lower()
+    print(f"\nContact list size is: {len(people)}")
+    action=input("\nDo you want to 'VIEW', 'ADD', 'CHANGE', 'DELETE', 'SEARCH' a person, or Enter 'q' to exit: ").lower()
     if action=='add':
         add_person()
-        print()
-        print("Person added!")
+        print("\nPerson added!")
     elif action=='delete':
         delete_person(people)
-        print()
-        print("Person deleted!")
+        print("\nPerson deleted!")
     elif action=='search':
         search_person(people)
     elif action=='view':
@@ -226,7 +191,5 @@ while True:
     elif action=='q':
         break
     else:
-        print()
-        print("Invalid response, try again")
+        print("\nInvalid response, try again")
     
-# print(people)
